bb854e4769bf30b976816cc74f0c3913cb7882dd
NatellaGomza <86558251+NatellaGomza@users.noreply.github.com>
2024-06-15 19:25:31 +0300

    code refactor

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let timer;
+let animation;
 let posX = -210;
 let posY = -210;
 let scale = 1;
@@ -10,32 +10,20 @@ let rotateRT = 5;
 let rotateRB = 360;
 let rotateBackRT = false;
 let rotateBackRB = false;
+const animationStart = Date.now();
+let progress = 0;
+let animate;
 
-const header1 = document.getElementsByClassName("header1");
-const header2 = document.getElementsByClassName("header2");
+const mainHeader = document.querySelector(".main-header");
+const header = document.querySelector(".header");
 const img1 = document.getElementsByClassName("img1");
 const img2 = document.getElementsByClassName("img2");
 const img3 = document.getElementsByClassName("img3");
 const img4 = document.getElementsByClassName("img4");
 
-function moveRight() {
-    posX += 5;
-    header1[0].style.left = posX + "px";
-}
-
-function moveDown() {
-    posY += 3;
-    header2[0].style.top = posY + "px";
-}
-
-function zoomInFont() {
-    scale += 0.02;
-    header2[0].style.transform = `scale(${scale})`
-}
-
 function rotateLeftTop() {
     rotateLT -= 0.5;
-    if(rotateLT === 300) {
+    if (rotateLT === 300) {
         rotateBack = true;
     }
     img1[0].style.transform = `rotate(${rotateLT + "deg"})`
@@ -48,7 +36,7 @@ function rotateLeftTopBack() {
 
 function rotateLeftBottom() {
     rotateLB += 0.5;
-    if(rotateLB === 160) {
+    if (rotateLB === 160) {
         rotateBackLB = true;
     }
     img4[0].style.transform = `rotate(${rotateLB + "deg"})`
@@ -61,7 +49,7 @@ function rotateLeftBottomBack() {
 
 function rotateRightBottom() {
     rotateRB -= 0.5;
-    if(rotateRB === 315) {
+    if (rotateRB === 315) {
         rotateBackRB = true;
     }
     img3[0].style.transform = `rotate(${rotateRB + "deg"})`
@@ -74,7 +62,7 @@ function rotateRightBottomBack() {
 
 function rotateRightTop() {
     rotateRT += 0.5;
-    if(rotateRT === 50) {
+    if (rotateRT === 50) {
         rotateBackRT = true;
     }
     img2[0].style.transform = `rotate(${rotateRT + "deg"})`
@@ -85,53 +73,77 @@ function rotateRightTopBack() {
     img2[0].style.transform = `rotate(${rotateRT + "deg"})`
 }
 
-function rotateRight() {
-
-}
-
-function animateH1() {
-    cancelAnimationFrame(timer);
-
+function animateHeader() {
     if (posY < 42) {
-        moveDown();
+        posY += 3;
+        header.style.top = posY + "px";
     }
 
     if (posY >= 42 && scale < 2.1) {
-        zoomInFont();
+        scale += 0.02;
+        header.style.transform = `scale(${scale})`
     }
-
-    if (posY >= 42 && scale >= 2.1 && posX <= 55) {
-        moveRight();
-
-    }
-
-    if (posY >= 42 && scale >= 2.1 && posX <= 60) {
-        if (rotateLT > 300 && !rotateBack) {
-            rotateLeftTop();
-        }
-        if (rotateBack && rotateLT < 345) {
-            rotateLeftTopBack();
-        }
-        if (rotateLB < 160 && !rotateBackLB) {
-            rotateLeftBottom();
-        }
-        if (rotateBackLB && rotateLB > 115) {
-            rotateLeftBottomBack();
-        }
-        if (rotateRT < 50 && !rotateBackRT) {
-            rotateRightTop();
-        }
-        if (rotateBackRT && rotateRT > 5) {
-            rotateRightTopBack();
-        }
-        if (rotateRB > 315 && !rotateBackRB) {
-            rotateRightBottom();
-        }
-        if (rotateBackRB && rotateRB < 360) {
-            rotateRightBottomBack();
-        }
-
-    }
-
-    timer = requestAnimationFrame(animateH1);
 }
+
+function animateMainHeader() {
+    if (posX <= 55) {
+        posX += 5;
+        mainHeader.style.left = posX + "px";
+    }
+}
+
+function rotatePicturesOutside() {
+    if (rotateLT > 300 && !rotateBack) {
+        rotateLeftTop();
+    }
+
+    if (rotateLB < 160 && !rotateBackLB) {
+        rotateLeftBottom();
+    }
+
+    if (rotateRT < 50 && !rotateBackRT) {
+        rotateRightTop();
+    }
+
+    if (rotateBackRB && rotateRB < 360) {
+        rotateRightBottomBack();
+    }
+}
+
+function rotatePicturesBack() {    
+    if (rotateBack && rotateLT < 345) {
+        rotateLeftTopBack();
+    }
+
+    if (rotateBackLB && rotateLB > 115) {
+        rotateLeftBottomBack();
+    }
+
+    if (rotateBackRT && rotateRT > 5) {
+        rotateRightTopBack();
+    }
+
+    if (rotateRB > 315 && !rotateBackRB) {
+        rotateRightBottom();
+    }
+}
+
+function animateBillBoard(timestamp) {
+    cancelAnimationFrame(animation);
+
+    animateHeader();
+
+    if (timestamp > 1000) {
+        animateMainHeader();
+        rotatePicturesOutside();
+        rotatePicturesBack();
+    }
+
+    if (timestamp < 2000) {
+        requestAnimationFrame(animateBillBoard);
+    } else {
+        cancelAnimationFrame(animateBillBoard);
+    }
+}
+
+animate = requestAnimationFrame(animateBillBoard);
